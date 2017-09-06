911e3d9f179b187bdfe3057083d734615de7ae7c
Kevin Paul Kalis <kevinpaulkalis@gmail.com>
2017-09-07 04:51:48 +0800

    Add title context

--- website/home/views.py
+++ website/home/views.py
@@ -3,3 +3,9 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "home/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+
+        context['siteTitle'] = "New Website"
+        return context
